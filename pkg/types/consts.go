25dc66f248bbaba95fa411155a4088e82ef095ad
cbluebird <crk2778355976@163.com>
2024-08-28 10:46:14 +0800

    pref: add containerd client in server
    Signed-off-by: cbluebird <crk2778355976@163.com>

--- pkg/types/consts.go
+++ pkg/types/consts.go
@@ -1,13 +1,16 @@
 package types
 
 const (
-	ContainerCommitOnStopEnvFlag        = "SEALOS_COMMIT_ON_STOP"
+	ContainerCommitOnStopEnvFlag = "SEALOS_COMMIT_ON_STOP"
+	ImageRegistryAddressOnEnv    = "SEALOS_COMMIT_IMAGE_REGISTRY"
+	ImageRegistryUserNameOnEnv   = "SEALOS_COMMIT_IMAGE_REGISTRY_USER"
+	ImageRegistryPasswordOnEnv   = "SEALOS_COMMIT_IMAGE_REGISTRY_PASSWORD"
+	ImageRegistryRepositoryOnEnv = "SEALOS_COMMIT_IMAGE_REGISTRY_REPOSITORY"
+	ImageSquashOnEnv             = "SEALOS_COMMIT_IMAGE_SQUASH"
+	ImageNameOnEnv               = "SEALOS_COMMIT_IMAGE_NAME"
+	SealosUsernameOnEnv          = "SEALOS_USER"
+)
+const (
+	ImageSquashOnEnvEnableValue         = "true"
 	ContainerCommitOnStopEnvEnableValue = "true"
-	ImageRegistryAddressOnEnv           = "SEALOS_COMMIT_IMAGE_REGISTRY"
-	ImageRegistryUserNameOnEnv          = "SEALOS_COMMIT_IMAGE_REGISTRY_USER"
-	ImageRegistryPasswordOnEnv          = "SEALOS_COMMIT_IMAGE_REGISTRY_PASSWORD"
-	ImageRegistryRepositoryOnEnv        = "SEALOS_COMMIT_IMAGE_REGISTRY_REPOSITORY"
-	ImageSquashOnEnv                    = "SEALOS_COMMIT_IMAGE_SQUASH"
-	ImageNameOnEnv                      = "SEALOS_COMMIT_IMAGE_NAME"
-	SealosUsernameOnEnv                 = "SEALOS_USER"
 )
